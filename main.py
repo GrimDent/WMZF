d0f3da6c68eaad88ec817ca8bea4da67fd7b92cd
Grim Dent <hcse49@gmail.com>
2020-11-27 18:49:09 +0100

    commit testowy 1

--- main.py
+++ main.py
@@ -1,16 +1,19 @@
-# This is a sample Python script.
+#-----------------------------------------------------------------------------------
 
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+#Projekt ze Wstępu do Modelowania Zjawisk Fizycznych
 
+#Autorzy: Kamil Sojka, Piotr Leymańczcyk
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+#-----------------------------------------------------------------------------------
 
-
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+#masa pierwszego obiektu
+m1=2
+#masa drugiego obiektu
+m2=1
+#odległość pomiędzy środkami ciężkości obiektów
+r=1
+#Stała grawitacji
+G=6.67430*10**(-11)
+#siła
+F=(G*m1*m2)/r**2
+print(F)
