4359ba6b22cb52601f25a8deb4d2aef6c51cfd83
Grim Dent <hcse49@gmail.com>
2020-11-27 19:49:48 +0100

    commit testowy 2

--- main.py
+++ main.py
@@ -9,7 +9,7 @@
 #masa pierwszego obiektu
 m1=2
 #masa drugiego obiektu
-m2=1
+m2=2
 #odległość pomiędzy środkami ciężkości obiektów
 r=1
 #Stała grawitacji
@@ -17,3 +17,9 @@ G=6.67430*10**(-11)
 #siła
 F=(G*m1*m2)/r**2
 print(F)
+#przyspieszenie pierwszego obiektu
+a1=(G*m2)/r**2
+print(a1)
+#przyspieszenie drugiego obiektu
+a2=(G*m1)/r**2
+print(a2)
