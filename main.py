a17066d02b7575fd65f394c3a20871dccf4b79d9
Grim Dent <hcse49@gmail.com>
2020-12-04 17:18:02 +0100

    commit 2.0

--- main.py
+++ main.py
@@ -5,22 +5,27 @@
 #Autorzy: Kamil Sojka, Piotr Leymańczcyk
 
 #-----------------------------------------------------------------------------------
-
-#masa pierwszego obiektu w kg
-m1=2000000000
-#promień pierwszego obiektu w m
-r1=1000000
-#masa drugiego obiektu w kg
-m2=3000000000
-#promień drugiego obiektu w m
-r2=2000000
-#odległość pomiędzy powirzchniami obiektów w m
-s=10000000
+plik=open('wyniki.txt', 'a')
+print("masa pierwszego obiektu w kg: ")
+#m1=2000000000
+m1=float(input())
+print("promień pierwszego obiektu w m: ")
+#r1=1000000
+r1=float(input())
+print("masa drugiego obiektu w kg: ")
+#m2=3000000000
+m2=float(input())
+print("promień drugiego obiektu w m: ")
+#r2=2000000
+r2=float(input())
+print("odległość pomiędzy powirzchniami obiektów w m: ")
+#s=10000000
+s=float(input())
 #Stała grawitacji
 G=6.67430*10**(-11)
 #siła oddziaływania grawitacyjnego między obiektami w
 F=(G*m1*m2)/s**2
-print('siła oddziaływania grawitacyjnego między obiektami na w t=0', F, 'Newtonów\n')
+print('siła oddziaływania grawitacyjnego między obiektami na dla t=0: ', F, 'Newtonów\n')
 #przyspieszenie pierwszego obiektu
 a1=(G*m2)/s**2
 print('przyspieszenie pierwszego obiektu wynosi', a1, 'metrów na sekundę do kwadratu\n' )
@@ -33,3 +38,19 @@ print('prędkość ciała 1 w chwili zderzenia wynosi', V1, 'metrów na sekundę
 #prędkość ciała 2 w chwili zderzenia
 V2=(G*m1)/(r1+r2)
 print('prędkość ciała 2 w chwili zderzenia wynosi', V2, 'metrów na sekundę\n')
+t=['dane wejściowe',
+'\nmasa1=',str(m1),
+'\npromień1=',str(r1),
+'\nmasa2=',str(m2),
+'\npromień2=',str(r2),
+'\nodległość=',str(s),
+
+'\n\ndane wyjściowe',
+'\nsiła grawitacyjna t=0 =',str(F),
+'\nprzyspiesznie1=',str(a1),
+'\nprzyspiesznie2=',str(a2),
+'\nprędkość w chwili zderzenia 1 =' ,str(V1),
+'\nprędkość w chwili zderzenia 2 =' ,str(V2),'\n\n']
+
+plik.writelines(t)
+plik.close()
